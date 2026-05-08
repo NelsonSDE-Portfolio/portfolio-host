a717b9abb55d798410523c577f4baa0130f15f30
Nelson Riera <149415820+NelsonSDE@users.noreply.github.com>
2026-05-08 12:42:48 -0600

    feat: wire ledger MFE remote into portfolio host (#8)

--- src/types/remotes.d.ts
+++ src/types/remotes.d.ts
@@ -4,3 +4,8 @@ declare module 'challengeTracker/App' {
   const App: React.ComponentType
   export default App
 }
+
+declare module 'ledger/App' {
+  const App: React.ComponentType
+  export default App
+}
